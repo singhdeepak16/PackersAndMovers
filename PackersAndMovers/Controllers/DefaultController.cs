85917a64a3bc03c7ccb04ae825be1ea3a8d35451
singhdeepak16 <deepak.ambro@gmail.com>
2023-08-03 14:56:29 +0530

    Add project files.

---
+++ PackersAndMovers/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PackersAndMovers.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: DefaultController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Default()
+        {
+            return View();
+        }
+
+    }
+}
